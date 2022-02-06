f0b6c080de4147293dbbc88afb2de15e84443531
Sam <beardmans72@gmail.com>
2022-02-06 22:53:46 +1300

    Install pwsh

--- src/install.sh
+++ src/install.sh
@@ -2,10 +2,20 @@
 apt update
 
 # Install base requirements
-apt install -y curl apt-transport-https ca-certificates gnupg
+apt install -y curl apt-transport-https ca-certificates gnupg wget software-properties-common
 
 # Install gcloud sdk
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt update
 apt install -y google-cloud-sdk
+
+# Install pwsh
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt update
+apt install -y powershell
+
+# Install terraform
+
+# Install terragrunt
