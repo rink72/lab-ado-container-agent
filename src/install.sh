29583e7d0188e377e41d5ec61f2d386620303e75
Sam <beardmans72@gmail.com>
2022-02-09 19:53:33 +1300

    Update options

--- src/install.sh
+++ src/install.sh
@@ -7,7 +7,7 @@ PWSH_VERSION=7.2.1-1.deb
 TERRAFORM_VERSION=1.1.5
 
 # Update apt
-apt-get update
+apt-get -qq update
 
 # Install base requirements
 apt-get -y -qq install curl apt-transport-https ca-certificates gnupg wget software-properties-common apt-utils
@@ -15,19 +15,19 @@ apt-get -y -qq install curl apt-transport-https ca-certificates gnupg wget softw
 # Install gcloud sdk
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt-get update
+apt-get -qq update
 apt-get -y -qq install google-cloud-sdk=$GOOGLE_CLOUD_SDK_VERSION
 
 # Install pwsh
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
-apt-get update
+apt-get -qq update
 apt-get -y -qq install powershell=$PWSH_VERSION
 
 # Install terraform
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update
+apt-get -qq update
 apt-get -y -qq install terraform=$TERRAFORM_VERSION
 
 # Install terragrunt
